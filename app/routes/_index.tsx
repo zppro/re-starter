ca725c814d71f9da0a2e2f865fa25671211a0511
parker <zppro@parkerdeMacBook-Air.local>
2024-11-13 11:20:54 +0800

    sidebar-07

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div className="h-screen container mx-auto grid grid-cols-4 place-content-center gap-4">
       <Link className="w-32 h-32 border place-content-center text-center" to="/concise">concise</Link>
-      <button className="w-32 h-32 border place-content-center text-center">sidebar-07</button>
+      <Link className="w-32 h-32 border place-content-center text-center" to="/sidebar-07">sidebar-07</Link>
     </div>
   )
 }
